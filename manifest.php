ec262f97c7ae70d38423a04bf0aca0549f0bdae4
Timothy Lim <limshenghwee@gmail.com>
2013-03-30 11:05:14 +1100

    Remove untested iOS launch images. Rename files

--- manifest.php
+++ manifest.php
@@ -2,10 +2,9 @@
 header("Content-Type: text/cache-manifest");
 ?>
 CACHE MANIFEST
-# rev 1.0 
+index.htm
 des_logo.png
 
 NETWORK:
-des.htm
-
-# <?php   echo "# Hash: " . md5_file('des.htm') . "\n"; ?>
+*
+<?php   echo "# Hash: " . md5_file('des.htm') . "\n"; ?>
